0145d5f1726023f886bf970d3aec89f40f0b976d
Mom Socheat <momsocheat60@gmail.com>
2025-10-14 10:53:48 +0700

    testing: make metadata public

--- WebRTCiOSSDK/api/VideoMetaData.swift
+++ WebRTCiOSSDK/api/VideoMetaData.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 public struct VideoMetaData: Codable {
-    var isMicMuted: Bool?
-    var isCameraOff: Bool?
-    var isScreenShare: Bool?
-    var userId: Int?
-    var username: String?
-    var profilePicture: String?
-    var role: String?
-    var entryId: Int?
+    public var isMicMuted: Bool?
+    public var isCameraOff: Bool?
+    public var isScreenShare: Bool?
+    public var userId: Int?
+    public var username: String?
+    public var profilePicture: String?
+    public var role: String?
+    public var entryId: Int?
     
-    static func create(_ payload: [String : Any]) -> Self? {
+    public static func create(_ payload: [String : Any]) -> Self? {
         guard let metaDataString = payload["metaData"] as? String,
               let data = metaDataString.data(using: .utf8) else {
             return nil
