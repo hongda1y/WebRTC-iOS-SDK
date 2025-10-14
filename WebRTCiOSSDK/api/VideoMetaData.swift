9cdeecabb1f532da2a08cc5fc1f88026da6be2ad
Mom Socheat <momsocheat60@gmail.com>
2025-10-14 10:57:01 +0700

    testing: make public init for metadata

--- WebRTCiOSSDK/api/VideoMetaData.swift
+++ WebRTCiOSSDK/api/VideoMetaData.swift
@@ -17,6 +17,8 @@ public struct VideoMetaData: Codable {
     public var role: String?
     public var entryId: Int?
     
+    public init() {}
+    
     public static func create(_ payload: [String : Any]) -> Self? {
         guard let metaDataString = payload["metaData"] as? String,
               let data = metaDataString.data(using: .utf8) else {
